bff3bc80fda4dc8a6faf647a2b97e2f17c906d4c
alanb <efik722life@gmail.com>
2025-06-05 12:34:49 +0900

    chore(ios): initialize ios app project

---
+++ ios/Bonsai/BonsaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonsaiApp.swift
+//  Bonsai
+//
+//  Created by alanb on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BonsaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/BonsaiTests/BonsaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  BonsaiTests.swift
+//  BonsaiTests
+//
+//  Created by alanb on 6/5/25.
+//
+
+import Testing
+@testable import Bonsai
+
+struct BonsaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
