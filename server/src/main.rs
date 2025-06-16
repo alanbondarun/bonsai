fc7e0353a2b05d173ccd531bb71ac5ba071389d7
alanb <efik722life@gmail.com>
2025-06-16 19:31:58 +0900

    feat: add hello world server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    axum::serve(listener, app()).await?;
+    Ok(())
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(|| async { "Hello, world!" }))
 }
